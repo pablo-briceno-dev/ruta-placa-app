88d51802550b5faad3173ba5a234e7868901ec30
pablo.briceno@clickonplans.com-Desarrollador <84730069+pablo-briceno-clickonplans@users.noreply.github.com>
2026-04-07 13:50:32 -0500

    updating

---
+++ android/app/src/main/kotlin/com/example/ruta_placa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ruta_placa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/pablobricenodev/ruta_placa/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.pablobricenodev.ruta_placa
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
